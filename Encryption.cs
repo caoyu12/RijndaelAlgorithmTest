319b6328fe0d90d1e9da682109d15062fd319d22
Cary Cao <caoyu12@gmail.com>
2022-07-11 22:38:17 +0800

    Add files via upload

---
+++ Encryption.cs
@@ -0,0 +1,49 @@
+﻿namespace RijndaelAlgorithmTest
+{
+    public static class Encryption
+    {
+        static string passPhrase = "P@ssPhr@se";        // can be any string
+        static string saltValue = "S@1tV@lue";          // can be any string
+        static string hashAlgorithm = "SHA1";           // can be "MD5"
+        static int passwordIterations = 2;              // can be any number
+        static string initVector = "@1B2c3D4e5F6g7H8";  // must be 16 bytes
+        static int keySize = 256;                       // can be 192 or 128
+
+        /// <summary>
+        /// Decrypts the data.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        public static string DecryptData(
+            string message)
+        {
+
+            return RijndaelAlgorithm.Decrypt(message,
+                                                 passPhrase,
+                                                 saltValue,
+                                                 hashAlgorithm,
+                                                 passwordIterations,
+                                                 initVector,
+                                                 keySize);
+        }
+
+        /// <summary>
+        /// Encyrpts the data.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        public static string EncyrptData(
+            string message)
+        {
+
+            return RijndaelAlgorithm.Encrypt(message,
+                                                        passPhrase,
+                                                        saltValue,
+                                                        hashAlgorithm,
+                                                        passwordIterations,
+                                                        initVector,
+                                                        keySize);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RijndaelAlgorithmTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input: Encrypt or Decrypt?");
+                Console.WriteLine("     1 - Encrypt Data");
+                Console.WriteLine("     2 - Decrypt Data");
+                var input = Console.ReadLine();
+                int i;
+                if (!int.TryParse(input, out i) || (i != 1 && i != 2))
+                {
+                    Console.WriteLine("Invalid input. Only accept 1 or 2!");
+                }
+                Console.WriteLine("Input the target text then press enter...");
+                var msg = Console.ReadLine();
+                if (i.Equals(1))
+                {
+                    Console.WriteLine("Encrypted text here:");
+                    Console.WriteLine(Encryption.EncyrptData(msg));
+                }
+                else
+                {
+                    try
+                    {
+                        var result = Encryption.DecryptData(msg);
+                        Console.WriteLine("Decrypted text here:");
+                        Console.WriteLine(result);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
